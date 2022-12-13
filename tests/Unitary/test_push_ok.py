b9401ca5695c66fa26e8218d21ef8f992e526b8b
MarianoRibas <marianoribas98@gmail.com>
2022-12-13 15:25:39 -0300

    Unitary_test_push_ok

--- tests/Unitary/test_push_ok.py
+++ tests/Unitary/test_push_ok.py
@@ -11,9 +11,17 @@ def test_client():
     context.push()
     
     return flask_app
-   
-def test_push_ok():
+
+@pytest.mark.parametrize('message, expected_result',[
+    ('Test msg 1','Test msg 1'),
+    (1,'1'),
+    ([],'[]'),
+])   
+def test_push_ok(message, expected_result):
     fake_redis = FakeStrictRedis()
-    result = push_item('Test Message',r = fake_redis)
+    result = push_item(message,r = fake_redis)
+    pushedItem = fake_redis.lpop('queue:messages')
+
     assert result['status'] == 'ok'
+    assert pushedItem.decode('utf-8') == expected_result
     
